d02baef18c1e534fe213f9c0feab31fe80074ba0
Mathieu LALA <mathieu.lala@epitech.eu>
2020-09-18 23:25:22 +0900

    chore: add generate tools script used in pipeline

---
+++ tools/generate.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+build_type=Debug
+arch=x64
+
+usage() {
+    cat << EOF
+Usage: $0 [-h|--help] [--build_type=Debug] [--arch=x64]
+
+Options:
+    -h|--help       Display this message.
+    --build_type    Set the build type of the CMake project.
+    --arch          Set the architecture for the CMake project.
+EOF
+    exit 2
+}
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -h|--help)
+    usage $#
+    shift
+    ;;
+    --build_type)
+    build_type="$2"
+    shift
+    shift
+    ;;
+    *)
+    shift
+    ;;
+esac
+done
+
+argument="-B build/$build_type -DCMAKE_BUILD_TYPE=$build_type"
+
+case "$(uname)" in
+"Linux" | "Darwin")
+  ;;
+*)
+   argument="$argument -A $arch"
+  ;;
+esac
+
+cmake $argument .
