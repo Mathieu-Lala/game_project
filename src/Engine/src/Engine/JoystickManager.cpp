ec754ba02d94e6b24c2f453a7a3e091fbe75e59d
Mathieu LALA <mathieu.lala@epitech.eu>
2020-10-01 14:57:40 +0900

    feat: refact joystick management + record joystick event

---
+++ src/Engine/src/Engine/JoystickManager.cpp
@@ -0,0 +1,125 @@
+#include "Engine/JoystickManager.hpp"
+#include "Engine/Core.hpp"
+
+engine::JoystickManager *engine::JoystickManager::s_instance{nullptr};
+
+engine::JoystickManager::JoystickManager()
+{
+    s_instance = this;
+
+    ::glfwSetJoystickCallback(callback_eventJoystickDetection);
+}
+
+auto engine::JoystickManager::getNextEvent() -> std::optional<Event>
+{
+    if (m_events.empty()) { return {}; }
+
+    auto nextEvent = m_events.front();
+    m_events.erase(m_events.begin());
+    return nextEvent;
+}
+
+auto engine::JoystickManager::add(const Joystick &j) -> void
+{
+    if (!get(j.id).has_value())
+        m_joysticks.push_back(j);
+    else
+        [[unlikely]] { spdlog::warn("engine::JoystickManager Trying to add an already existing joystick"); }
+}
+
+auto engine::JoystickManager::get(int id) -> std::optional<Joystick *const>
+{
+    if (const auto it = std::find_if(m_joysticks.begin(), m_joysticks.end(), [id](auto &i) { return i.id == id; });
+        it != m_joysticks.end()) {
+        return it.base();
+    }
+    return {};
+}
+
+auto engine::JoystickManager::poll() -> void
+{
+    pollAxis();
+    pollButtons();
+}
+
+auto engine::JoystickManager::remove(const Joystick &j) -> void
+{
+    if (auto it = std::find_if(m_joysticks.begin(), m_joysticks.end(), [id = j.id](auto &i) { return i.id == id; });
+        it != m_joysticks.end()) {
+        m_joysticks.erase(it);
+    } else
+        [[unlikely]] { spdlog::warn("engine::JoystickManager Trying to remove an none existing joystick"); }
+}
+
+auto engine::JoystickManager::update(const Moved<JoystickAxis> &j) -> void
+{
+    if (auto it = std::find_if(m_joysticks.begin(), m_joysticks.end(), [id = j.source.id](auto &i) { return i.id == id; });
+        it != m_joysticks.end()) {
+        it->axes[j.source.axis] = j.source.value;
+    }
+}
+
+auto engine::JoystickManager::update(const Pressed<JoystickButton> &j) -> void
+{
+    if (auto it = std::find_if(m_joysticks.begin(), m_joysticks.end(), [id = j.source.id](auto &i) { return i.id == id; });
+        it != m_joysticks.end()) {
+        it->buttons[j.source.button] = true;
+    }
+}
+
+auto engine::JoystickManager::update(const Released<JoystickButton> &j) -> void
+{
+    if (auto it = std::find_if(m_joysticks.begin(), m_joysticks.end(), [id = j.source.id](auto &i) { return i.id == id; });
+        it != m_joysticks.end()) {
+        it->buttons[j.source.button] = false;
+    }
+}
+
+auto engine::JoystickManager::each(const std::function<void(const Joystick &)> &f) -> void
+{
+    for (const auto &i : m_joysticks) f(i);
+}
+
+auto engine::JoystickManager::callback_eventJoystickDetection(int id, int event) -> void
+{
+    if (Core::Holder().instance->getEventMode() == Core::RECORD) {
+        if (event == GLFW_CONNECTED) {
+            s_instance->m_events.emplace_back(Connected<Joystick>{{id}});
+        } else if (event == GLFW_DISCONNECTED) {
+            s_instance->m_events.emplace_back(Disconnected<Joystick>{{id}});
+        }
+    }
+}
+
+auto engine::JoystickManager::pollAxis() -> void
+{
+    int count = 0;
+    for (auto &joy : m_joysticks) {
+        const auto axes = ::glfwGetJoystickAxes(joy.id, &count);
+        for (std::uint32_t i = 0; i != static_cast<std::uint32_t>(count); i++) {
+            if (std::abs(joy.axes[i] - axes[i]) >= 0.05f) {
+                this->m_events.emplace_back(
+                    Moved<JoystickAxis>({joy.id, magic_enum::enum_cast<Joystick::Axis>(i).value(), axes[i]}));
+            }
+        }
+    }
+}
+
+auto engine::JoystickManager::pollButtons() -> void
+{
+    int count = 0;
+    for (auto &joy : m_joysticks) {
+        const auto buttons = ::glfwGetJoystickButtons(joy.id, &count);
+        for (std::uint32_t i = 0; i != static_cast<std::uint32_t>(count); i++) {
+            if (joy.buttons[i] != static_cast<bool>(buttons[i])) {
+                if (buttons[i]) {
+                    this->m_events.emplace_back(
+                        Pressed<JoystickButton>({joy.id, magic_enum::enum_cast<Joystick::Buttons>(i).value()}));
+                } else {
+                    this->m_events.emplace_back(
+                        Released<JoystickButton>({joy.id, magic_enum::enum_cast<Joystick::Buttons>(i).value()}));
+                }
+            }
+        }
+    }
+}
