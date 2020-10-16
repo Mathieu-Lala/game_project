1390c130cbc93da9017f7720847ed25c2206a316
Pierre-Genest <pge@outlook.fr>
2020-10-16 07:07:21 +0200

    [Fix] Boss in ennemy factory

--- src/Application/include/entity/EnemyFactory.hpp
+++ src/Application/include/entity/EnemyFactory.hpp
@@ -10,6 +10,7 @@ namespace game {
 class EnemyFactory {
 public:
     static void FirstEnemy(entt::registry &world, engine::Shader *, const glm::vec2 &pos);
+    static void Boss(entt::registry &world, engine::Shader *, const glm::vec2 &pos);
 };
 
 } // namespace game
