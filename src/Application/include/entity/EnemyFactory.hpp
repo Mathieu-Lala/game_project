84380b18d5e2235c21be5446ef8397f1eb386fd6
Pierre-Genest <55022949+Pierre-Genest@users.noreply.github.com>
2020-10-18 10:16:12 +0200

    Feat/hud2.0 (#41)
    * [ADD] non dynamic HUD
    
    * [FIX] crash but no boss implementation yet
    
    * [Fix] compilation linux
    
    * [Add] enemy boss modif
    
    * [FIX] compilation linuxx
    
    * [Fix] conflict merge
    
    Co-authored-by: Pierre-Genest <pge@outlook.fr>
    Co-authored-by: Mathieu Lala <mathieu.lala@epitech.eu>

--- src/Application/include/entity/EnemyFactory.hpp
+++ src/Application/include/entity/EnemyFactory.hpp
@@ -9,6 +9,7 @@ namespace game {
 
 class EnemyFactory {
 public:
+    static void Boss(entt::registry &world, const glm::vec2 &pos);
     static void FirstEnemy(entt::registry &world, const glm::vec2 &pos);
 };
 
