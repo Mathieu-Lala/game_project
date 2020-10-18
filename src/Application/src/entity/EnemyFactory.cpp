f8043a5bbb0ff301efb08e5da71b03bdfda5f385
Pierre-Genest <pge@outlook.fr>
2020-10-18 07:54:29 +0200

    [Fix] conflict merge

--- src/Application/src/entity/EnemyFactory.cpp
+++ src/Application/src/entity/EnemyFactory.cpp
@@ -4,13 +4,9 @@
 #include "entity/EnemyFactory.hpp"
 #include "EntityDepth.hpp"
 
-<<<<<<< HEAD
 using namespace std::chrono_literals; // ms ..
 
-auto game::EnemyFactory::FirstEnemy(entt::registry &world, engine::Shader *shader, const glm::vec2 &pos) -> void
-=======
 auto game::EnemyFactory::FirstEnemy(entt::registry &world, const glm::vec2 &pos) -> void
->>>>>>> 2ef832090ceee2b5f1494879689d876d5e658aae
 {
     const auto e = world.create();
     world.emplace<entt::tag<"enemy"_hs>>(e);
@@ -27,7 +23,7 @@ auto game::EnemyFactory::FirstEnemy(entt::registry &world, const glm::vec2 &pos)
     world.emplace<Health>(e, 50.0f, 50.0f);
 }
 
-auto game::EnemyFactory::Boss(entt::registry &world, engine::Shader *shader, const glm::vec2 &pos) -> void
+auto game::EnemyFactory::Boss(entt::registry &world, const glm::vec2 &pos) -> void
 {
     const auto e = world.create();
     world.emplace<entt::tag<"boss"_hs>>(e);
@@ -35,7 +31,7 @@ auto game::EnemyFactory::Boss(entt::registry &world, engine::Shader *shader, con
     world.emplace<engine::d2::Velocity>(e, 0.01 * (std::rand() & 1), 0.01 * (std::rand() & 1));
     world.emplace<engine::d2::Scale>(e, 3.0, 3.0);
     world.emplace<engine::d2::HitboxSolid>(e, 3.0, 3.0);
-    world.emplace<engine::Drawable>(e, engine::DrawableFactory::rectangle({1, 0, 0})).shader = shader;
+    world.emplace<engine::Drawable>(e, engine::DrawableFactory::rectangle());
     world.emplace<game::ViewRange>(e, 10.0f);
     world.emplace<game::AttackRange>(e, 3.0f);
     world.emplace<game::AttackCooldown>(e, false, 2000ms, 0ms);
