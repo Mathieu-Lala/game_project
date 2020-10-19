7220eef6545aa30ccbe83138f2792f05e8fea11f
yanis fourel <yanis.fourel@epitech.eu>
2020-10-19 19:30:32 +0900

    fixed warning

--- src/Engine/include/Engine/audio/AlErrorHandling.hpp
+++ src/Engine/include/Engine/audio/AlErrorHandling.hpp
@@ -15,20 +15,20 @@ namespace engine {
 #    define DEBUG_BREAK
 #endif // _MSC_VER
 
-#define alcCall(device, call)                      \
-    do {                                           \
-        while (alcGetError(device) != AL_NO_ERROR) \
-            ;                                      \
-        call;                                      \
-        if (checkForAlcError(device)) DEBUG_BREAK; \
+#define alcCall(device, call)                          \
+    do {                                               \
+        while (alcGetError(device) != AL_NO_ERROR)     \
+            ;                                          \
+        call;                                          \
+        if (checkForAlcError(device)) { DEBUG_BREAK; } \
     } while (0);
 
-#define alCall(call)                        \
-    do {                                    \
-        while (alGetError() != AL_NO_ERROR) \
-            ;                               \
-        call;                               \
-        if (checkForAlError()) DEBUG_BREAK; \
+#define alCall(call)                            \
+    do {                                        \
+        while (alGetError() != AL_NO_ERROR)     \
+            ;                                   \
+        call;                                   \
+        if (checkForAlError()) { DEBUG_BREAK; } \
     } while (0);
 
 bool checkForAlcError(ALCdevice *device);
