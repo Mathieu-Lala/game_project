7ef04b68192ff8613a1668357c62fcb83a9568fa
Benjamin Praud <benjamin.praud@epitech.eu>
2020-11-17 12:30:15 +0900

    fix: Display piercing_arrow and sword_attack

--- src/Application/src/factory/SpellFactory.cpp
+++ src/Application/src/factory/SpellFactory.cpp
@@ -50,7 +50,7 @@ auto game::SpellFactory::create<game::SpellFactory::SWORD_ATTACK>(entt::registry
     const auto &caster_pos = world.get<engine::d3::Position>(caster);
     const auto &attack_damage = world.get<game::AttackDamage>(caster);
 
-    auto color = world.has<entt::tag<"enemy"_hs>>(caster) ? glm::vec3{0, 1, 0} : glm::vec3{1, 1, 0.5};
+    auto color = world.has<entt::tag<"enemy"_hs>>(caster) ? glm::vec3{0, 1, 0} : glm::vec3{0.3, 0.7, 0.2};
     
     holder.instance->getAudioManager().getSound(holder.instance->settings().data_folder + "sounds/fire_cast.wav")->play();
     const auto spell = world.create();
@@ -60,6 +60,7 @@ auto game::SpellFactory::create<game::SpellFactory::SWORD_ATTACK>(entt::registry
     world.emplace<engine::Drawable>(spell, engine::DrawableFactory::rectangle());
     engine::DrawableFactory::fix_color(world, spell, std::move(color));
     world.emplace<engine::d3::Position>(spell, caster_pos.x + direction.x, caster_pos.y + direction.y, -1.0);
+    world.emplace<engine::d2::Rotation>(spell, 0.f);
     world.emplace<engine::d2::Scale>(spell, 0.7, 0.7);
     world.emplace<engine::d2::HitboxFloat>(spell, 0.7, 0.7);
     world.emplace<engine::Source>(spell, caster);
@@ -94,7 +95,6 @@ auto game::SpellFactory::create<game::SpellFactory::FIREBALL>(entt::registry &wo
     world.emplace<engine::d2::Scale>(spell, 0.7, 0.7);
     world.emplace<engine::d2::HitboxFloat>(spell, 0.7, 0.7);
     world.emplace<engine::Source>(spell, caster);
-    spdlog::info("done");
     return spell;
 }
 
@@ -116,14 +116,14 @@ auto game::SpellFactory::create<game::SpellFactory::PIERCING_ARROW>(entt::regist
     const auto spell = world.create();
     world.emplace<entt::tag<"spell"_hs>>(spell);
     world.emplace<game::Lifetime>(spell, 1000ms);
-    world.emplace<game::AttackDamage>(spell, attack_damage);
+    world.emplace<game::AttackDamage>(spell, attack_damage.damage * 1.25f);
     world.emplace<engine::Drawable>(spell, engine::DrawableFactory::rectangle());
-    engine::DrawableFactory::fix_color(world, spell, {0.3, 0.3, 1});
+    engine::DrawableFactory::fix_color(world, spell, {0.6, 0, 0.3});
     world.emplace<engine::d3::Position>(spell, caster_pos.x + direction.x, caster_pos.y + direction.y, -1.0); // note : why -1
     world.emplace<engine::d2::Velocity>(spell, direction.x * speed , direction.y * speed);
+    world.emplace<engine::d2::Rotation>(spell, 0.f);
     world.emplace<engine::d2::Scale>(spell, 0.7, 0.7);
     world.emplace<engine::d2::HitboxFloat>(spell, 0.7, 0.7);
     world.emplace<engine::Source>(spell, caster);
-    spdlog::info("done");
     return spell;
 }
