22248baeead5d6352c8cfa700f3a3a32c385abb2
yanis fourel <yanis.fourel@epitech.eu>
2020-12-04 17:17:13 +0900

    Tree display

--- src/Application/include/menu/UpgradePanel.hpp
+++ src/Application/include/menu/UpgradePanel.hpp
@@ -15,10 +15,22 @@ public:
     void event(entt::registry &world, ThePURGE &game, const engine::Event &e) final;
 
 private:
+    struct ClassTreeNode {
+        const Class *cl;
+        ImVec2 relPos; // Position of the center of the element relative to tree bounds
+        std::vector<ClassTreeNode> children;
+    };
+
+private:
+
     void drawTree(entt::registry &world, ThePURGE &game) noexcept;
+    auto getTreeDrawPos(const ImVec2 &relPos, float elemSize) const noexcept -> ImVec2;
+
     void drawDetailPanel(entt::registry &world, ThePURGE &game) noexcept;
 
     void updateClassTree(entt::registry &world, ThePURGE &game);
+    auto generateTreeRec(ThePURGE &game, const Class *cl, int depth = 0) const noexcept -> ClassTreeNode;
+    auto findInTree(const Class *cl) const noexcept -> const ClassTreeNode *;
 
     auto isOwned(const Class *) const noexcept -> bool;
     auto isPurchaseable(const Class *) const noexcept -> bool;
@@ -29,12 +41,28 @@ private:
 
 
     const Class *m_selectedClass;
+    ImVec2 m_treeSelectionCurrentPos; // in tree unit (@see `ClassTreeNode`)
+    ImVec2 m_treeSelectionDestinationPos;
 
     // Classes by tier
     std::vector<std::vector<const Class *>> m_classes;
 
     std::vector<const Class *> m_owned;
     std::vector<const Class *> m_purchaseable;
+
+    ClassTreeNode m_root;
+private:
+    static inline constexpr auto kHeightMargins = 100; 
+    static inline constexpr auto kWidthMargins = 150;
+
+    static inline const ImVec2 treeTopLeft{524 + kWidthMargins, 223 + kHeightMargins};
+    static inline const ImVec2 treeSize{1344 - 2 * kWidthMargins, 835 - 2 * kHeightMargins};
+
+    static inline constexpr auto kIconSize = 100;
+    static inline constexpr auto kFrameSize = 250;
+    static inline constexpr auto kCursorSize = 200;
+
+    static inline constexpr float kSelectionAnimationSpeed = 0.5f;
 };
 
 } // namespace game
--- src/Application/src/menu/UpgradePanel.cpp
+++ src/Application/src/menu/UpgradePanel.cpp
@@ -137,178 +137,45 @@ void game::menu::UpgradePanel::drawDetailPanel(entt::registry &world, ThePURGE &
 
 void game::menu::UpgradePanel::drawTree(entt::registry &, ThePURGE &) noexcept
 {
-    /*
+    std::vector<const ClassTreeNode *> todo = {&m_root};
 
-    static bool choosetrigger = false;
-    static auto test = game.dbClasses().getStarterClass();
-    static std::optional<Class> selectedClass;
-    static std::string chosenTrig = "";
-    static int spellmapped = 0;
-    static int infoAdd;
+    while (!todo.empty()) {
+        const auto current = todo.back();
+        todo.pop_back();
 
-    static auto texture = [data = holder.instance->settings().data_folder]() {
-        // clang-format off
-            const auto path = std::to_array({
-                data + "/textures/InfoHud.png",
-                data + "/textures/CPoint.PNG",
-                data + "/textures/validate.png",
-                data + "/textures/controller/LB.png",
-                data + "/textures/controller/LT.png",
-                data + "/textures/controller/RT.png",
-                data + "/textures/controller/RB.png"
-            });
-        // clang-format on
-        std::array<std::uint32_t, path.size()> out;
-        std::generate(out.begin(), out.end(), [&path, n = 0ul]() mutable {
-            const auto &o = engine::Core::Holder{}.instance->getCache<engine::Texture>().load<engine::LoaderTexture>(
-                entt::hashed_string{fmt::format("resource/texture/identifier/{}", path.at(n)).data()}, path.at(n));
-            n++;
-            return o->id;
-        });
-        return out;
-    }();
-
-    ImVec2 size = ImVec2(1000.0f, 1000.0f);
-    ImGui::SetNextWindowPos(ImVec2(game.getCamera().getCenter().x + size.x / 2, 0));
-    ImGui::SetNextWindowSize(ImVec2(size.x, size.y));
-    ImGui::Begin(
-        "Evolution Panel",
-        nullptr,
-        ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoResize
-            | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoMove);
-    size = ImGui::GetWindowSize();
-    ImGui::Image(reinterpret_cast<void *>(static_cast<intptr_t>((texture[0]))), ImVec2(size.x - 30, size.y - 10));
-    ImGui::SetCursorPos(ImVec2(0, 200));
-    ImVec2 next;
-    if (selectedClass.has_value()) {
-        ImVec2 icon = ImVec2(ImGui::GetCursorPosX() + size.x / 3 + 9, ImGui::GetCursorPosY());
-        ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + size.x / 3 + 9, ImGui::GetCursorPosY()));
-        helper::ImGui::Text("Class Name: {}", selectedClass->name);
-        ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + size.x / 3, ImGui::GetCursorPosY()));
-        helper::ImGui::Text("Class description: {}", selectedClass->description);
-        if (infoAdd == 1) {
-            static std::size_t triggerValue = 5;
-            std::optional<Classes> lastclass = world.get<Classes>(player);
-            if (spellmapped % 2 != 0 && lastclass.value().ids.size() > 1
-                && lastclass.value().ids[lastclass.value().ids.size() - 1] == selectedClass.value().name) {
-                ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + size.x / 3, ImGui::GetCursorPosY() + 50));
-                ImGui::Text("Choose Your trigger :");
-                ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + size.x / 3, ImGui::GetCursorPosY() + 10));
-                auto &spell = world.get<SpellSlots>(player);
-                if (ImGui::ImageButton(reinterpret_cast<void *>(static_cast<intptr_t>((texture[3]))), ImVec2(50, 50))) {
-                    chosenTrig = "LB";
-                    triggerValue = 0;
-                }
-                ImGui::SameLine();
-                if (ImGui::ImageButton(reinterpret_cast<void *>(static_cast<intptr_t>((texture[4]))), ImVec2(50, 50))) {
-                    chosenTrig = "LT";
-                    triggerValue = 2;
-                }
-                ImGui::SameLine();
-                if (ImGui::ImageButton(reinterpret_cast<void *>(static_cast<intptr_t>((texture[5]))), ImVec2(50, 50))) {
-                    chosenTrig = "RT";
-                    triggerValue = 3;
-                }
-                ImGui::SameLine();
-                if (ImGui::ImageButton(reinterpret_cast<void *>(static_cast<intptr_t>((texture[6]))), ImVec2(50, 50))) {
-                    chosenTrig = "RB";
-                    triggerValue = 1;
-                }
-                ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + size.x / 3, ImGui::GetCursorPosY() + 10));
-                helper::ImGui::Text("You choosed the trigger: {}", chosenTrig.c_str());
-                ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + size.x / 3, ImGui::GetCursorPosY()));
-                if (chosenTrig != "") {
-                    if (ImGui::Button("Validate")) {
-                        choosetrigger = false;
-                        auto NewSpell = game.dbSpells().instantiate(selectedClass.value().spells[0]).value();
-                        for (auto i = 0ul; i < spell.spells.size(); i++) {
-                            if (spell.spells[i].has_value() && NewSpell.id == spell.spells[i].value().id) {
-                                spell.spells[i] = {};
-                            }
-                        }
-                        spell.spells[triggerValue] = NewSpell;
-                        spellmapped++;
-                    }
-                }
-            } else {
-                ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + size.x / 3, ImGui::GetCursorPosY()));
-                ImGui::Text("Already bought");
-            }
-            next = ImVec2(ImGui::GetCursorPosX(), ImGui::GetCursorPosY());
-            ImGui::SetCursorPos(ImVec2(icon.x - 50, icon.y));
-            ImGui::Image(reinterpret_cast<void *>(static_cast<intptr_t>((texture[2]))), ImVec2(50, 50));
-        } else if (infoAdd == 2) {
-            if (skillPoints >= selectedClass.value().cost) {
-                if (choosetrigger == true) {
-                    ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + size.x / 3, ImGui::GetCursorPosY()));
-                    ImGui::Text("You haven't choose a trigger for the spell");
-                }
-                ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + size.x / 3, ImGui::GetCursorPosY()));
-                if (ImGui::Button("Add class")) {
-                    game.logics()->onPlayerPurchase.publish(world, player, selectedClass.value());
-                    infoAdd = 1;
-                    if (choosetrigger == false) {
-                        spellmapped++;
-                        choosetrigger = true;
-                    } else
-                        spellmapped = spellmapped + 2;
-                }
-            } else {
-                ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + size.x / 3, ImGui::GetCursorPosY() + 10));
-                helper::ImGui::Button("No skill point", ImVec4(0.5f, 0.5f, 0.5f, 0.5f));
-            }
-
-        } else {
-            ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + size.x / 3, ImGui::GetCursorPosY()));
-            ImGui::Text("You can't buy it yet");
+        for (const auto &child : current->children) {
+            ImGui::GetWindowDrawList()->AddLine(
+                getTreeDrawPos(current->relPos, 0), getTreeDrawPos(child.relPos, 0), ImGui::ColorConvertFloat4ToU32(ImVec4(0, 0, 0, 1)), 3.f);
+            todo.push_back(&child);
         }
+
+        std::uint32_t glowTexture;
+
+        if (isOwned(current->cl))
+            glowTexture = helper::getTexture("menus/upgrade_panel/tree/frames/owned.png");
+        else if (isPurchaseable(current->cl))
+            glowTexture = helper::getTexture("menus/upgrade_panel/tree/frames/buyable.png");
+        else
+            glowTexture = helper::getTexture("menus/upgrade_panel/tree/frames/unavailable.png");
+
+        helper::drawTexture(glowTexture, getTreeDrawPos(current->relPos, kFrameSize), ImVec2(kFrameSize, kFrameSize));
+
+        const auto iconTexture = helper::getTexture(current->cl->iconPath);
+        helper::drawTexture(iconTexture, getTreeDrawPos(current->relPos, kIconSize), ImVec2(kIconSize, kIconSize));
     }
-    // Competences
-    ImGui::SetCursorPos(ImVec2(size.x / 2 - 17 * ImGui::GetFontSize() / 2, size.y / 2));
-    helper::ImGui::Text("Point de comp: {}", skillPoints); // rendre dynamique le nombre de point de comp
-    ImGui::SameLine(0.0f, ImGui::GetStyle().ItemInnerSpacing.x);
-    ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX(), ImGui::GetCursorPosY() - 5));
-    ImGui::Image(reinterpret_cast<void *>(static_cast<intptr_t>((texture[1]))), ImVec2(20, 20));
 
-    // Classes tree
-    float length = size.x / 2 - (12.0f + static_cast<float>(test.name.length())) * ImGui::GetFontSize();
-    ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + length, ImGui::GetCursorPosY() + 30));
-    helper::ImGui::Text("class Name: {}", test.name);
+    m_treeSelectionCurrentPos.x = std::lerp(m_treeSelectionCurrentPos.x, m_treeSelectionDestinationPos.x, kSelectionAnimationSpeed);
+    m_treeSelectionCurrentPos.y = std::lerp(m_treeSelectionCurrentPos.y, m_treeSelectionDestinationPos.y, kSelectionAnimationSpeed);
 
-    std::vector<std::string> classes = {game.dbClasses().getStarterClass().name};
-    std::vector<std::string> purchasable;
+    const auto cursorTexture = helper::getTexture("menus/upgrade_panel/tree/cursor.png");
+    helper::drawTexture(cursorTexture, getTreeDrawPos(m_treeSelectionCurrentPos, kCursorSize), ImVec2(kCursorSize, kCursorSize));
+}
 
-    while (!classes.empty()) {
-        std::vector<std::string> children;
-        ImGui::SetCursorPos(ImVec2(ImGui::GetCursorPosX() + size.x / 4, ImGui::GetCursorPosY() + 10));
-        for (const auto &id : classes) {
-            const auto &data = *game.dbClasses().getByName(id);
+auto game::menu::UpgradePanel::getTreeDrawPos(const ImVec2 &relPos, float elemSize) const noexcept -> ImVec2
+{
+    const ImVec2 centerAs1080p(treeTopLeft.x + treeSize.x * relPos.x, treeTopLeft.y + treeSize.y * relPos.y);
 
-            if (std::find(already_purchased.begin(), already_purchased.end(), id) != already_purchased.end()) {
-                purchasable.insert(purchasable.end(), data.children.begin(), data.children.end());
-                if (helper::ImGui::Button(data.name, ImVec4(0, 1, 0, 0.5))) {
-                    selectedClass = data;
-                    infoAdd = 1;
-                }
-            } else if (std::find(purchasable.begin(), purchasable.end(), id) != purchasable.end()) {
-                if (helper::ImGui::Button(data.name, ImVec4(1, 1, 0, 0.5))) {
-                    selectedClass = data;
-                    infoAdd = 2;
-                }
-            } else {
-                if (helper::ImGui::Button(data.name, ImVec4(1, 0, 0, 0.5))) {
-                    selectedClass = data;
-                    infoAdd = 3;
-                }
-            }
-            ImGui::SameLine();
-            children.insert(children.end(), data.children.begin(), data.children.end());
-        }
-        classes = std::move(children);
-        ImGui::Text(" "); // ImGui::NextLine()
-    }
-    ImGui::End();
-    */
+    return helper::frac2pixel(helper::from1080p(centerAs1080p.x - elemSize * 0.5f, centerAs1080p.y - elemSize * 0.5f));
 }
 
 void game::menu::UpgradePanel::updateClassTree(entt::registry &world, ThePURGE &game)
@@ -344,6 +211,56 @@ void game::menu::UpgradePanel::updateClassTree(entt::registry &world, ThePURGE &
     }
 
     printClassTreeDebug();
+
+    m_root = generateTreeRec(game, m_classes[0][0]);
+
+    m_treeSelectionDestinationPos = findInTree(m_selectedClass)->relPos;
+    m_treeSelectionCurrentPos = m_treeSelectionDestinationPos;
+}
+
+auto game::menu::UpgradePanel::generateTreeRec(ThePURGE &game, const Class *cl, int depth) const noexcept -> ClassTreeNode
+{
+    float yPos = static_cast<float>(depth) / (m_classes.size() - 1);
+
+    if (depth == m_classes.size() - 1) {
+        const auto &lastRow = m_classes.back();
+        const auto iter = std::find(std::begin(lastRow), std::end(lastRow), cl);
+        auto idx = std::distance(std::begin(lastRow), iter);
+
+        float xPos = static_cast<float>(idx) / (lastRow.size() - 1);
+
+        return ClassTreeNode{.cl = cl, .relPos = ImVec2(xPos, yPos), .children = {}};
+    }
+
+    ClassTreeNode result;
+
+    result.cl = cl;
+    result.relPos.y = yPos;
+    result.relPos.x = 0;
+
+    for (const auto &childName : cl->children) {
+        result.children.push_back(generateTreeRec(game, game.dbClasses().getByName(childName), depth + 1));
+        result.relPos.x += result.children.back().relPos.x / cl->children.size();
+    }
+
+    return result;
+}
+
+auto game::menu::UpgradePanel::findInTree(const Class *cl) const noexcept -> const ClassTreeNode *
+{
+    std::vector<const ClassTreeNode *> todo = {&m_root};
+
+    while (!todo.empty()) {
+        const auto current = todo.back();
+        if (current->cl == cl) return current;
+
+        todo.pop_back();
+
+        for (const auto &child : current->children) todo.push_back(&child);
+    }
+
+    assert(!"The requested class does not exist in the tree");
+    return nullptr; // guaranteed crash
 }
 
 bool game::menu::UpgradePanel::isOwned(const Class *cl) const noexcept
@@ -351,9 +268,9 @@ bool game::menu::UpgradePanel::isOwned(const Class *cl) const noexcept
     return std::find(std::begin(m_owned), std::end(m_owned), cl) != std::end(m_owned);
 }
 
-auto game::menu::UpgradePanel::isPurchaseable(const Class *) const noexcept -> bool
+auto game::menu::UpgradePanel::isPurchaseable(const Class *cl) const noexcept -> bool
 {
-    return std::find(std::begin(m_purchaseable), std::end(m_purchaseable), m_selectedClass) != std::end(m_purchaseable);
+    return std::find(std::begin(m_purchaseable), std::end(m_purchaseable), cl) != std::end(m_purchaseable);
 }
 
 void game::menu::UpgradePanel::printClassTreeDebug() const noexcept
